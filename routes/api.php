37ba0be726fe28e0604a3ee343d0a25d61c8f194
Arpit <arpit.mishra@microprixs.in>
2025-12-12 17:35:53 +0530

    API versioning + API resource

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,28 @@
 <?php
 
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\Api\V1\StoryController as V1StoryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+Route::get('/hello',function(){
+    return ["message" => "Laravel Backend API Development!"];
+});
+
+
+// Route::get('/posts',[PostController::class,'index'])->name('posts.index');
+// Route::post('/posts',[PostController::class,'store'])->name('posts.store');
+// Route::get('/posts/{id}',[PostController::class,'show'])->name('posts.show');
+
+
+
+Route::prefix('v1')->group(function(){
+    Route::apiResource('/stories',V1StoryController::class);
+});
+
+
